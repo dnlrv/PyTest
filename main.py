38a55acb9a783ad6ee5e39d63575f260db21f696
Dan Rivera <87451188+dnlrv@users.noreply.github.com>
2025-06-18 14:32:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import requests
+import json
 
 maintreeurl  = "https://api.github.com/repos/dnlrv/PyTest/git/trees/main?recursive=1"
 basecripturl = "https://raw.githubusercontent.com/dnlrv/PyTest/main/"
 
 maintree = requests.get(maintreeurl)
+
+pyPaths = [i['path'] for i in (json.loads(maintreeurl.text))['tree'] if i['path'].endswith('.py')]
+
+for s in pyPaths:
+  exec(requests.get(f'{basecripturl}{s}')).content.decode('utf-8')
