4de9b0c73e8846ec5402a86e0d3558368cf4bb03
Dan Rivera <87451188+dnlrv@users.noreply.github.com>
2025-07-14 14:58:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import json
 tree_url = 'https://api.github.com/repos/dnlrv/PyTest/git/trees/main?recursive=1'
 maintree = requests.get(tree_url)
 tree_data = json.loads(maintree.text)
-#print(tree_data)
 py_paths = [
     entry['path']
     for entry in tree_data.get('tree', [])
@@ -15,7 +14,5 @@ py_paths = [
 ]
 
 for s in py_paths:
-    #print(f'I am getting https://raw.githubusercontent.com/dnlrv/PyTest/main/{s}')
     script = requests.get(f'https://raw.githubusercontent.com/dnlrv/PyTest/main/{s}').content.decode('utf-8')
-    #print(script)
     exec(script)
