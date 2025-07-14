b68e7a745055f43dab6852f770bb04760bfc89ab
Dan Rivera <87451188+dnlrv@users.noreply.github.com>
2025-07-14 14:51:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+# import requests; exec(requests.get('https://raw.githubusercontent.com/dnlrv/PyTest/refs/heads/main/example.py').content.decode('utf-8'))
+
+import requests
 import json
 
-maintreeurl  = "https://api.github.com/repos/dnlrv/PyTest/git/trees/main?recursive=1"
-basecripturl = "https://raw.githubusercontent.com/dnlrv/PyTest/main/"
+tree_url = 'https://api.github.com/repos/dnlrv/PyTest/git/trees/main?recursive=1'
+maintree = requests.get(tree_url)
+tree_data = json.loads(maintree.text)
+print(tree_data)
+py_paths = [
+    p for p in tree_data['tree']
+    if p['path'].endswith('.py') and p['path'] != 'main.py'
+]
 
-maintree = requests.get(maintreeurl)
-
-pyPaths = [i['path'] for i in (json.loads(maintree.text))['tree'] if i['path'].endswith('.py')]
-
-for s in pyPaths:
-  exec(requests.get(f'{basecripturl}{s}')).content.decode('utf-8')
+for s in py_paths:
+    print(f'I am getting https://raw.githubusercontent.com/dnlrv/PyTest/main/{s}')
+    script = requests.get(f'https://raw.githubusercontent.com/dnlrv/PyTest/main/{s}').content.decode('utf-8')
+    print(script)
+    exec(script)
